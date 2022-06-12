5fcf3cb5a22d11ab4cd6ffb3457dc8b2fcf8dfa0
faruk ayaz <46282512+frkyz07@users.noreply.github.com>
2022-06-12 22:27:18 +0300

    -- taking screenshot with listeners

--- E2EProject2/src/test/java/Academy/homepage.java
+++ E2EProject2/src/test/java/Academy/homepage.java
@@ -3,6 +3,7 @@ package Academy;
 import java.io.IOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
@@ -13,6 +14,7 @@ import pageObjects.LoginPage;
 import resources.base;
 
 public class homepage extends base {
+	public WebDriver driver;
 	public static Logger log = LogManager.getLogger(base.class.getName());
 	@BeforeTest
 	public void initialize() throws IOException
--- E2EProject2/src/test/java/Academy/validateTitle.java
+++ E2EProject2/src/test/java/Academy/validateTitle.java
@@ -8,11 +8,13 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
 
 import pageObjects.LandingPage;
 import resources.base;
 
 public class validateTitle extends base {
+	public WebDriver driver;
 	public static Logger log = LogManager.getLogger(base.class.getName());
 
 	@BeforeTest
